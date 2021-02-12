1e681aa61887416c076040a41c8044785950e418
darshanwj <darshanchaturanga@gmail.com>
2021-02-13 03:22:17 +0400

    func tests with db init

--- tests/test_alch.py
+++ tests/test_alch.py
@@ -1,11 +1,11 @@
-""" Tests """
+"""Tests."""
 import pytest
 from alch import create_app
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def app():
-    """ Setup test app """
+    """Setup test app."""
     app = create_app(
         {
             'TESTING': True,
@@ -13,16 +13,37 @@ def app():
         }
     )
 
-    yield app
+    # clear the test db
+    from alch.db import Session, Base
+    for table in reversed(Base.metadata.sorted_tables):
+        Session.execute('TRUNCATE TABLE {}'.format(table))
+    Session.commit()
 
-    # clear test db here
+    return app
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def client(app):
     """A test client for the app."""
     return app.test_client()
 
 
+def test_create_user(client):
+    """Test creating a user"""
+    res = client.post(
+        '/user',
+        json={
+            'name': 'first test user'
+        }
+    )
+    body = res.get_json()
+    assert body is not None
+    assert 'user' in body
+    user = body['user']
+    assert 'id' in user and user['id'] == 1
+    assert 'name' in user and user['name'] == 'first test user'
+
+
 def test_home(client):
+    """Test home page."""
     assert client.get("/").status_code == 200
